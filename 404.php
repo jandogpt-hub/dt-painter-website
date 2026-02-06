0860fecfd88b5d34381ac5377be6bc70d581a24a
Jando <jandogpt@gmail.com>
2026-02-05 22:50:35 -0800

    feat: Add .htaccess for URL rewriting, GZIP compression, browser caching, and a custom 404 error page.

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = 'Page Not Found - DT Painter';
+$pageDescription = 'The page you are looking for could not be found.';
+include 'includes/header.php';
+?>
+
+<section class="section">
+    <div class="container text-center">
+        <h1>404 - Page Not Found</h1>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+        <a href="/" class="btn btn-primary">Go Back Home</a>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
